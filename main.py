28167e5f7067c98b6e14c56a76799c669bbac513
shahkaran281 <shahkaran281@gmail.com>
2023-10-05 19:38:46 -0700

    Implementation

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ import subprocess
 if __name__ == "__main__":
     while True:
         user_input = input("Write Command: ")
-        # print("User Input:", user_input)
         if user_input.lower().startswith('create'):
             command = user_input[len('create'):].strip()
-            # print("Command:", command)
             subprocess.run(['python3', 'create.py', command])
         elif user_input.lower().startswith('insert'):
             command = user_input[len('insert'):].strip()
