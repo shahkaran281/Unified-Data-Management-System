a07fafa311afedf783d9f010423696c4ab05cb17
shahkaran281 <shahkaran281@gmail.com>
2023-10-09 16:18:08 -0700

    Spanning Implementated

--- main.py
+++ main.py
@@ -2,20 +2,40 @@ import re
 import sys
 import subprocess
 
+subprocesses = []
+
+
+def run_subprocess(command):
+    try:
+        subprocess_obj = subprocess.Popen(command)
+        subprocesses.append(subprocess_obj)
+        subprocess_obj.wait()
+    except Exception as e:
+        print(f"Error running subprocess: {str(e)}")
+
+
 if __name__ == "__main__":
-    while True:
-        user_input = input("Write Command: ")
-        if user_input.lower().startswith('create'):
-            command = user_input[len('create'):].strip()
-            subprocess.run(['python3', 'create.py', command])
-        elif user_input.lower().startswith('insert'):
-            command = user_input[len('insert'):].strip()
-            subprocess.run(['python3', 'insert.py', command])
-        elif user_input.lower().startswith('update'):
-            command = user_input[len('update'):].strip()
-            subprocess.run(['python3', 'update.py', command])
-        elif user_input.lower().startswith('delete'):
-            command = user_input[len('delete'):].strip()
-            subprocess.run(['python3', 'delete.py', command])
-        elif user_input.lower() == 'exit':
-            break
+    try:
+        while True:
+            user_input = input("Write Command: ")
+            if user_input.lower().startswith('create'):
+                command = ['python3', 'create.py'] + \
+                    user_input[len('create'):].strip().split()
+                run_subprocess(command)
+            elif user_input.lower().startswith('insert'):
+                command = ['python3', 'insert.py'] + \
+                    user_input[len('insert'):].strip().split()
+                run_subprocess(command)
+            elif user_input.lower().startswith('update'):
+                command = ['python3', 'update.py'] + \
+                    user_input[len('update'):].strip().split()
+                run_subprocess(command)
+            elif user_input.lower().startswith('delete'):
+                command = ['python3', 'delete.py'] + \
+                    user_input[len('delete'):].strip().split()
+                run_subprocess(command)
+            elif user_input.lower() == 'exit':
+                break
+    finally:
+        for subprocess_obj in subprocesses:
+            subprocess_obj.terminate()
