88acc3d4f7195100575e544f6194fba2de34feb8
Aniket Kumar <32923119+aniketkr97@users.noreply.github.com>
2023-10-09 18:25:13 -0700

    simple crud

--- create.py
+++ create.py
@@ -1,16 +1,27 @@
 import sys
 import re
-
+import os
+import csv
 
 def create_table(table_name, attributes):
+    try:
+        print(f"Creating table")
+        print("TableName:", table_name)
+        print("Attributes:", attributes)
+        attributes.insert(0 , 'uuid')
+        path = f"data/{table_name}.csv"
+        if os.path.exists(path):
+            raise FileExistsError("Table Exists Already")
+        with open(path , 'w' , newline= '') as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(attributes)
+    except FileExistsError as e:
+        print(f"Error: {e}")
 
-    print(f"Creating table")
-    print("TableName:", table_name)
-    print("Attributes:", attributes)
+
 
 
 if __name__ == "__main__":
-
     command = sys.argv[1]
     matchTable = re.match(r'TABLE (.+)', command, re.IGNORECASE)
     if matchTable:
