c3f5650a4b8fdf0ca4e215db539ce02091317020
Aniket Kumar <32923119+aniketkr97@users.noreply.github.com>
2023-10-09 18:25:13 -0700

    simple crud

--- insert.py
+++ insert.py
@@ -1,11 +1,21 @@
 import sys
 import re
-
+import uuid
+import os
+import csv
 
 def insert_table(tableName, values):
     print("Insert table called")
     print("Table Name:", tableName)
     print("Value List:", values)
+    path = f"data/{table_name}.csv"
+    values.insert(0 , uuid.uuid4())
+    if os.path.exists(path) == False:
+            raise FileExistsError("Table Does not Exist")
+    with open(path , 'a' , newline= '') as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(values)
+
 
 
 if __name__ == "__main__":
