bfc67ff3ce883ed231d29fbdb61ac0bd0761e7a9
shahkaran281 <shahkaran281@gmail.com>
2023-11-03 12:42:39 -0700

    Join space issue fixed

--- join.py
+++ join.py
@@ -2,6 +2,7 @@ import re
 import sys
 import csv
 
+
 def join_and_print_selected_columns(file1, file2, key1='uuid', key2='fk', columns1=[], columns2=[]):
     # Create dictionaries to store the data from each CSV file
     data1 = {}
@@ -40,29 +41,36 @@ def join_and_print_selected_columns(file1, file2, key1='uuid', key2='fk', column
     columns2 = [col.strip('\'') for col in columns2]
 
     # Check if all columns from input exist in the data
-    missing_columns1 = [col for col in columns1 if col not in data1.get(next(iter(data1), {}))]
-    missing_columns2 = [col for col in columns2 if col not in data2.get(next(iter(data2), {}))]
+    missing_columns1 = [
+        col for col in columns1 if col not in data1.get(next(iter(data1), {}))]
+    missing_columns2 = [
+        col for col in columns2 if col not in data2.get(next(iter(data2), {}))]
 
     if missing_columns1:
-        print(f"Columns {', '.join(missing_columns1)} not found in table {file1}.")
+        print(
+            f"Columns {', '.join(missing_columns1)} not found in table {file1}.")
         return
     if missing_columns2:
-        print(f"Columns {', '.join(missing_columns2)} not found in table {file2}.")
+        print(
+            f"Columns {', '.join(missing_columns2)} not found in table {file2}.")
         return
 
     # Perform the join and print selected columns from both tables
     for key in data1:
         if key in data2:
             combined_data = {**data1[key], **data2[key]}
-            selected_data1 = [combined_data.get(column, '') for column in columns1]
-            selected_data2 = [combined_data.get(column, '') for column in columns2]
+            selected_data1 = [combined_data.get(
+                column, '') for column in columns1]
+            selected_data2 = [combined_data.get(
+                column, '') for column in columns2]
             print(", ".join(selected_data1) + ", " + ", ".join(selected_data2))
+
 
 if __name__ == '__main__':
     # Your input string
     command = sys.argv[1]
     # "['Youtuber'], ['video_views_rank','country_rank'] FROM YoutubeChannels.uuid = YoutubeViews.fk"
-    pattern = r'\[(.*?)\],\s+\[(.*?)\]\s+FROM\s+(\S+)\s*\.(\S+)\s*=\s*(\S+)\s*\.(\S+)'
+    pattern = r'\[(.*?)\],\s*\[(.*?)\]\s+FROM\s+(\S+)\s*\.(\S+)\s*=\s*(\S+)\s*\.(\S+)'
     match = re.match(pattern, command, re.IGNORECASE)
 
     if match:
@@ -78,6 +86,8 @@ if __name__ == '__main__':
         columns2 = [col.strip('\'').strip() for col in columns2_str.split(',')]
 
         # Call the join_and_print_selected_columns function
-        join_and_print_selected_columns(table1, table2, col1, col2, columns1, columns2)
+        join_and_print_selected_columns(
+            table1, table2, col1, col2, columns1, columns2)
     else:
-        print("No match found in the input string. PLEASE USE: JOIN [COLUMNS_FROM_TABLE1_SEPARATED_BY_COMMA], [COLUMNS_FROM_TABLE2_SEPARATED_BY_COMMA] FROM TABLE1.COL1 = TABLE2.COL2")
+        print(
+            "No match found in the input string. PLEASE USE: JOIN [COLUMNS_FROM_TABLE1_SEPARATED_BY_COMMA], [COLUMNS_FROM_TABLE2_SEPARATED_BY_COMMA] FROM TABLE1.COL1 = TABLE2.COL2")
