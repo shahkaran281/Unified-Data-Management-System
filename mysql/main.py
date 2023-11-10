6496f58a7b8ad9c2a289021706b671463f9e2e85
Aniket Kumar <32923119+aniketkr97@users.noreply.github.com>
2023-11-09 17:14:51 -0800

    SQL FIXED with minor bugs

--- mysql/main.py
+++ mysql/main.py
@@ -4,7 +4,7 @@ import subprocess
 
 if __name__ == "__main__":
     while True:
-        user_input = input("Write Command: ")
+        user_input = input("Write Command: ").strip()
         if user_input.lower().startswith('create'):
             command = user_input[len('create'):].strip()
             subprocess.run(['python3', 'create.py', command])
@@ -17,6 +17,15 @@ if __name__ == "__main__":
         elif user_input.lower().startswith('delete'):
             command = user_input[len('delete'):].strip()
             subprocess.run(['python3', 'delete.py', command])
+        elif user_input.lower().startswith('join'):
+            command = user_input[len('join'):].strip()
+            subprocess.run(['python3', 'join.py', command])
+        elif user_input.lower().startswith('proj'):
+            command = user_input[len('proj'):].strip()
+            subprocess.run(['python3', 'proj.py', command])
+        elif user_input.lower().startswith('filter'):
+            command = user_input[len('filter'):].strip()
+            subprocess.run(['python3', 'filter.py', command])
         elif user_input.lower().startswith('aggregate'):
             command = user_input[len('aggregate'):].strip()
             subprocess.run(['python3', 'aggregate.py', command])
